3ca1cba4090dd0eb83163d71ff9bda60a5da524f
Michau <botkotel@gmail.com>
2025-05-25 17:49:55 +0200

    Zakutalizowano główną funkcje - dodano obsługę Swaggera

--- music-store-api/main.go
+++ music-store-api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"music-store-api/config"
 	"music-store-api/controllers"
+	_ "music-store-api/docs"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -13,13 +16,21 @@ func main() {
 
 	r := gin.Default()
 
+	r.Static("/static", "./static")
+	r.GET("/", func(c *gin.Context) {
+		c.File("./static/index.html")
+	})
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	albumRoutes := r.Group("/albums")
 	{
 		albumRoutes.GET("", controllers.GetAlbums)
 		albumRoutes.GET("/:id", controllers.GetAlbumByID)
 		albumRoutes.POST("", controllers.CreateAlbum)
-		albumRoutes.PUT("/:id", controllers.UpdateAlbum)
+		albumRoutes.PATCH("/:id", controllers.UpdateAlbum)
 		albumRoutes.DELETE("/:id", controllers.DeleteAlbum)
 	}
-	r.Run(":8080")
+
+	r.Run(":25565")
 }
