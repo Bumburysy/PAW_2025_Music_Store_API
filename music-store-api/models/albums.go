c6b875c96fd9bf4e522c9e1bb2b7ad7447519326
Michau <botkotel@gmail.com>
2025-05-29 18:32:30 +0200

    Przebudowano strukturę kolekcji albums

--- music-store-api/models/albums.go
+++ music-store-api/models/albums.go
@@ -1,12 +1,19 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Album struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Title  string             `bson:"title" json:"title"`
-	Artist string             `bson:"artist" json:"artist"`
-	Genre  string             `bson:"genre" json:"genre"`
-	Price  float64            `bson:"price" json:"price"`
-	Stock  int                `bson:"stock" json:"stock"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title     string             `bson:"title" json:"title"`
+	Artist    string             `bson:"artist" json:"artist"`
+	Genre     string             `bson:"genre" json:"genre"`
+	Price     float64            `bson:"price" json:"price"`
+	Quantity  int                `bson:"quantity" json:"quantity"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	CoverURL  string             `bson:"cover_url" json:"cover_url"`
 }
