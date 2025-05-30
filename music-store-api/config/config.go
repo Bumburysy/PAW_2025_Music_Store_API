1657dcceb4d7242517629817fd738a2805105a57
Michau <botkotel@gmail.com>
2025-05-30 16:35:18 +0200

    Dodano funkcje rozłączenia z bazą po zakończeniu działania programu

--- music-store-api/config/config.go
+++ music-store-api/config/config.go
@@ -10,6 +10,7 @@ import (
 )
 
 var DB *mongo.Database
+var Client *mongo.Client
 
 func ConnectDB() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -20,13 +21,24 @@ func ConnectDB() {
 		log.Fatalf("Błąd łączenia z MongoDB: %v", err)
 	}
 
-	// Sprawdzenie połączenia (ping)
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatalf("MongoDB ping error: %v", err)
 	}
 
 	DB = client.Database("PAW-API-Database")
+	Client = client
 
 	log.Println("MongoDB connected!")
+}
+
+func DisconnectDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := Client.Disconnect(ctx); err != nil {
+		log.Printf("Błąd rozłączania z MongoDB: %v", err)
+	} else {
+		log.Println("MongoDB disconnected!")
+	}
 }
